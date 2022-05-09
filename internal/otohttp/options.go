30769ae9a6d989d4d4ada55a641f57ce5fec4fa2
Michael Henderson <mdhender@mdhender.com>
2022-05-08 21:16:06 -0600

    added blank line to comment

--- internal/otohttp/options.go
+++ internal/otohttp/options.go
@@ -1,6 +1,7 @@
 /*
  * queenie - a spelling bee helper
  * Copyright (C) 2022 Michael D Henderson
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
